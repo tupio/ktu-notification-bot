cf48b6c01dcbb4b4bfd430964a9838e40bcf67a5
Denil C Verghese <denilverghese@gmail.com>
2021-03-30 18:46:43 +0530

    Update v1.1
    - Reformatted the code  & fixes #7
    - Merged code from heroku
    - Added vscode variables
    - Update LICENSE
    - fixes #5

--- scrapper.py
+++ scrapper.py
@@ -1,3 +1,26 @@
+# MIT License
+
+# Copyright (c) 2021 Denil C Verghese
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 import time
 import hashlib
 import requests
@@ -11,18 +34,20 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import re
 # __________________________ CONFIG AREA _______________________
 
-debug = True                                                             # This will prevent sending messages to channel during development
-sleep_time =300 #seconds.                                                # Time  delay it will wake and and check for new notification
+# This will prevent sending messages to channel during development
+debug = True
+
+# Time  delay it will wake and and check for new notification
+sleep_time = 300  # seconds.
 
 # Whatsapp forward message
-header = "🔈 "
-footer2 =  ".\n ------------------------- \nShared from *KTU Notification Channel*"\
-    " \n👉 Join Now!  https://t.me/KTU_RC \n⚡️ *No Ads* \n🔸 *Share to your friends* "
+wa_header = "🔈 "
+wa_footer = ".\n ------------------------- \nShared from *KTU Notification Channel* \n👉 Join Now!  https://t.me/KTU_RC \n⚡️ *No Ads* \n🔸 *Share to your friends* "
 
 # Telegram Message
-footer =  "---\n 👉 Join: https://t.me/KTU\\_RC \n🔸 No Ads⚡️"
-button_text  = "Visit KTU 🌐"                                               
-button_link = "https://ktu.edu.in/eu/core/announcements.htm" 
+tl_footer = "---\n 👉 Join: https://t.me/KTU\\_RC \n🔸 No Ads⚡️"
+button_text = "Visit KTU 🌐"
+button_link = "https://ktu.edu.in/eu/core/announcements.htm"
 
 # _________________________  END OF CONFIG _________________________
 
@@ -35,95 +60,96 @@ PRIVATE_ID = os.environ['PRIVATE_ID']
 DATABASE_URL = os.environ['DATABASE_URL']
 STATUS = os.environ['STATUS']
 
-add_url = "https://ktu.edu.in"
+url_prefix = "https://ktu.edu.in"
 url = "https://ktu.edu.in/eu/core/announcements.htm"
 
 try:
-	if "DEBUG" in STATUS:
-		debug=True
-	else:
-		debug=False
+    debug = "DEBUG" in STATUS
 except:
-	pass
+    pass
 
 
 def log(message):
     print(message)
-	
-def getlist(cur):
+
+
+def bot_log(message):
+    bot.send_message(chat_id=PRIVATE_ID, text=message,
+                     disable_notification=False,
+                     parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+
+def get_msg_hashes(cur):
     cur.execute("SELECT * FROM hashes;")
-    data =convert(cur.fetchall())
+    data = convert_to_list(cur.fetchall())
     log(str(data))
-    if (data is None and not debug) or ( len(data)==0 and not debug):
-        bot.send_message(chat_id=PRIVATE_ID, text="*Read From DB Failed*", disable_notification=False,
-                         parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True)
+    if (data is None or len(data) == 0) and not debug:
+        bot_log("*Read From DB Failed*")
         exit(1)
     elif debug:
         return []
     return data
 
-def save_msg_hash(listi,conn,cur):
-    log("Saving :" + str(listi))
+
+def save_msg_hash(hashes, conn, cur):
+    log("Saving :" + str(hashes))
     if debug:
         cur.execute("delete FROM hashes;")
-        for hash_val in listi:
-            cur.execute("INSERT INTO hashes VALUES (%s)", (hash_val, ))
+        for _hash in hashes:
+            cur.execute("INSERT INTO hashes VALUES (%s)", (_hash, ))
             conn.commit()
     else:
-        if listi != None:
-            cur.execute("INSERT INTO hashes VALUES (%s)", (listi, ))
+        if hashes != None:
+            cur.execute("INSERT INTO hashes VALUES (%s)", (hashes, ))
             conn.commit()
     log("Saved")
 
-def convert(tip):
-	data =[]
-	for x in tip:
-			data.append(x[0])
-	return data		
 
+# converts result tuples into a dict
+def convert_to_list(tip):
+    data = []
+    for x in tip:
+        data.append(x[0])
+    return data
+
+
+# removes link text from the bottom position since it will be added as buttons
 def remove_last(s, old, new, occurrence):
     li = s.rsplit(old, occurrence)
     return new.join(li)
 
+# Builds the button menu for links
 def build_menu(buttons, n_cols):
     menu = [buttons[sd:sd + n_cols] for sd in range(0, len(buttons), n_cols)]
     return menu
 
-def send_notification(notification, link_list4, enable_preview_i):
-    disable_preview = False
-    if enable_preview_i is None:
-        disable_preview = True
-        enable_preview_i = " "
+
+def send_notification(notification, button_list, enable_preview_i):
+    disable_preview = enable_preview_i in "\n\n-"
+    to = PRIVATE_ID
+    if not debug:
+        to = CHANNEL_ID
     try:
-        if debug:
-            bot.send_message(chat_id=PRIVATE_ID, text= notification + enable_preview_i + footer,
-                     reply_markup=InlineKeyboardMarkup(build_menu(link_list4, n_cols=2)),
-                     disable_web_page_preview=disable_preview, parse_mode=telegram.ParseMode.MARKDOWN)
-        if not debug:
-            bot.send_message(chat_id=CHANNEL_ID , text=notification + enable_preview_i + footer, parse_mode=telegram.ParseMode.MARKDOWN,
-                        reply_markup=InlineKeyboardMarkup(build_menu(link_list4, n_cols=2)),
-                        disable_web_page_preview=disable_preview)
+        bot.send_message(chat_id=to, text=notification +
+                         enable_preview_i + tl_footer, parse_mode=telegram.ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
+                             build_menu(button_list, n_cols=2)),
+                         disable_web_page_preview=disable_preview)
         log("Message Sent")
         return True
     except telegram.error.TimedOut:
         log("Timeout trying again")
-        if debug:
-            bot.send_message(chat_id=PRIVATE_ID, text= notification  + enable_preview_i + footer,
-                     reply_markup=InlineKeyboardMarkup(build_menu(link_list4, n_cols=2)),
-                     disable_web_page_preview=disable_preview, parse_mode=telegram.ParseMode.MARKDOWN)
-        if not debug:
-            bot.send_message(chat_id=CHANNEL_ID , text=notification  + enable_preview_i + footer, parse_mode=telegram.ParseMode.MARKDOWN,
-                        reply_markup=InlineKeyboardMarkup(build_menu(link_list4, n_cols=2)),
-                        disable_web_page_preview=disable_preview)
+        bot.send_message(chat_id=to, text=notification +
+                         enable_preview_i + tl_footer, parse_mode=telegram.ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
+                             build_menu(button_list, n_cols=2)),
+                         disable_web_page_preview=disable_preview)
         log("Message Sent")
         return True
     except telegram.error.BadRequest as e:
-        bot.send_message(chat_id=PRIVATE_ID, text= e.message ,
-                    parse_mode=telegram.ParseMode.MARKDOWN)
-        bot.send_message(chat_id=PRIVATE_ID, text= "\n\n `" + notification  + enable_preview_i + footer + "`" ,
-                    parse_mode=telegram.ParseMode.MARKDOWN)
+        bot_log(e.message)
+        bot_log("\n\n `" + notification + enable_preview_i + tl_footer + "`")
         log(e.message)
         return False
+
 
 def find_links(links, body):
     link_list = [InlineKeyboardButton(button_text, url=button_link)]
@@ -132,31 +158,34 @@ def find_links(links, body):
         if "ktu.edu" in link["href"] or "http" in link["href"][:10] or "www." in link["href"]:
             temp_link = link["href"]
         else:
-            temp_link = add_url + link["href"]
+            temp_link = url_prefix + link["href"]
         link_list.append(InlineKeyboardButton(link.text, url=temp_link))
-    return body,link_list
+    return body, link_list
+
 
 def get_first_pdf_link(links):
     if links is not None:
         for link in links:
-            if ".pdf" in link["url"]:
-                return link["url"]
-    return None
+            if ".pdf" in link["url"] or ".jpg" in link["url"]:
+                temp = str(link["url"])\
+                    .replace("(", "%28")\
+                    .replace(")", "%29")  # encodes the '(' and ')'
+                return "\n\n[-]("+temp+")"
+    return "\n\n-"
 
-def main_function(conn,cur):
-    hash_list = getlist(cur)
+
+def fetcher(conn, cur):
+    hash_list = get_msg_hashes(cur)
     log("Connecting to website")
     try:
         website = requests.get(url).text
     except:
         log("Unable to access Website!")
-        bot.send_message(chat_id=PRIVATE_ID, text="*Unable to accesss Website*", disable_notification=False,
-                         parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True)
-    
+        bot_log("*Unable to accesss Website*")
+
     if "Page Not Found" in website:
         log("Page not found")
-        bot.send_message(chat_id=PRIVATE_ID, text="*Unable to accesss Website*", disable_notification=False,
-                         parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True)
+        bot_log("*Unable to accesss Website*")
         exit(2)
     else:
         log("Connected Successfully")
@@ -165,73 +194,69 @@ def main_function(conn,cur):
     all_notification = soup.findAll('tr')
 
     i = 0
-    msg_hash_list =[]
+    msg_hash_list = []
     for item in all_notification:
         contents = item.findAll('li')
 
-        comments = contents[0].findAll(text=lambda text:isinstance(text, Comment))
+        comments = contents[0].findAll(
+            text=lambda text: isinstance(text, Comment))
         [comment.extract() for comment in comments]
-        
+
         contents = str(contents)\
-            .replace("<br>","\n")\
-                .replace("<p>","\n")\
-                    .replace("</p>","")\
-                        .replace("<b>","*")\
-                            .replace("</b>","*\n\n")\
-                                .replace("[","")\
-                                    .replace("]","")\
-                                        .replace("_", "\\_")
-                                        
-                           
-                                   
-        temmmm=  BeautifulSoup(contents,'html.parser')   
-        body = ""                                  
-        for x in temmmm.find("li").findAll(text=True):
-            body = body +  x
-        body , links = find_links(item.findAll('a', href=True), body=body)
-        body = body.replace("**","").strip()
-        body = re.sub('\n+','\n',body)
+            .replace("<br>", "\n")\
+            .replace("<p>", "\n")\
+            .replace("</p>", "")\
+            .replace("<b>", "*")\
+            .replace("</b>", "*\n\n")\
+            .replace("[", "")\
+            .replace("]", "")\
+            .replace("_", "\\_")
+
+        souped = BeautifulSoup(contents, 'html.parser')
+        body = ""
+        for x in souped.find("li").findAll(text=True):
+            body = body + x
+        body, links = find_links(item.findAll('a', href=True), body=body)
+        body = body.replace("**", "").strip()
+        body = re.sub('\n+', '\n', body)
         links.append(InlineKeyboardButton("WhatsApp It!", "https://api.whatsapp.com/send?&text=" + urllib.parse.quote(
-            header + body +  footer2)))
-        pdf_link = get_first_pdf_link(links)
-        footer_pdf = None
-        if pdf_link is not None:
-            footer_pdf  = "\n\n[-]("+pdf_link+")" 
+            wa_header + body + wa_footer)))
+        footer_pdf = get_first_pdf_link(links)
         if i < 10:
             msg_hash = (hashlib.md5(body.encode('utf-8')).hexdigest())
             if not debug:
-            	if not msg_hash in hash_list:
-                    send_notification(header + body, links, footer_pdf)
-                    save_msg_hash(msg_hash,conn,cur)
+                if not msg_hash in hash_list:
+                    send_notification(wa_header + body, links, footer_pdf)
+                    save_msg_hash(msg_hash, conn, cur)
             if debug:
-                if not send_notification(header + body, links, footer_pdf):
+                if not send_notification(wa_header + body, links, footer_pdf):
                     log("Message will not be sent")
                 msg_hash_list.append(msg_hash)
         else:
             break
         i = i + 1
     if debug:
-        save_msg_hash(msg_hash_list,conn,cur)
-    
+        save_msg_hash(msg_hash_list, conn, cur)
+
 
 def main():
     if not debug:
         while True:
             log("Scanning for new")
-            log("connecting to DB" )
+            log("connecting to DB")
             conn = psycopg2.connect(DATABASE_URL, sslmode='require')
             cur = conn.cursor()
             log("Loading function")
-            main_function(conn,cur)
+            fetcher(conn, cur)
             log("Closing DB")
             cur.close()
             conn.close()
             log("Scan complete")
-            time.sleep(sleep_time)            
+            time.sleep(sleep_time)
     else:
         conn = psycopg2.connect(DATABASE_URL, sslmode='require')
         cur = conn.cursor()
-        main_function(conn,cur)
+        fetcher(conn, cur)
         try:
             cur.close()
             conn.close()
@@ -243,10 +268,9 @@ if __name__ == '__main__':
     if not debug:
         try:
             main()
-        except Exception as e :
+        except Exception as e:
             log("main failed " + str(e))
             pass
     if debug:
         log("Debug mode")
         main()
-        
